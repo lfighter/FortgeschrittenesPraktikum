8a4a82528d70aeb879dbbdfb86f05296dcffc976
Lukas Rolf <lukas.rolf@online.de>
2018-06-17 03:05:53 +0200

    kp

--- 04-Versuch18/scripts/mainscriptw.py
+++ 04-Versuch18/scripts/mainscriptw.py
@@ -60,19 +60,14 @@ import random
 #print(f2(AW, BW, CW))
 #print(error_to_tex(f,'f',[AW, BW, CW], [A, B, C],[A, B]))
 
-a=7.31 #cm
-r=2.25 #cm
-omegaDurch4PI = (1-a/np.sqrt(a**2+r**2))/2
-print(omegaDurch4PI)
-AnzahlAnTagen=unp.uarray(6462,1) #days
-HalbwertsZeit=unp.uarray(4943,5) #days
-AktivitätEu=unp.uarray(4130,60) #bq
-AktivitätEu=1/2**(AnzahlAnTagen/HalbwertsZeit) * AktivitätEu
-print(AktivitätEu)
+
 
 
 def gaus(x, a, c,sigma,b):
     return a* np.exp(-(x-b)**2/(2*sigma**2))+c
+
+def Line(x, a, b):
+    return a* x+b
 
 xWerte = range(0,100)
 E = np.array(xWerte)+0.1
@@ -127,6 +122,7 @@ def gausFitMitPlot(Werte, ranges, name):
     return unp.uarray(AllParams,AllCovar)
 
 
+#######################
 #print(gausFitMitPlot(E,[[0,25]],'test'))
 
 #for i in range(len(bs)):
@@ -136,11 +132,15 @@ def gausFitMitPlot(Werte, ranges, name):
 #    print(gausFitMitPlot(E,[[np.max(np.array([int(bs[i]-sigmas[i]*5)-1,1])),np.min(np.array([int(bs[i]+sigmas[i]*5)+1,len(E)]))]],'test'))
 
 #print(gausFitMitPlot(E,[[0,100]],'test'))
-#             0         0         0        121       244       295       344        0         411         443         678         778         867         964        1005        1085        1112        1212        1299        1408         1457
-ranges = [[100,115],[115,126],[205,230],[300,320],[605,625],[730,750],[853,870],[910,930],[1020,1035],[1100,1118],[1700,1730],[1925,1950],[2145,2170],[2370,2425],[2480,2520],[2680,2720],[2740,2790],[3000,3030],[3200,3255],[3460,3540],[3605,3650]]
+
+#####################################################kali
+
+
+#             0         0         0        121       244       295       344       367        411         443         678         778         867         964        1005        1085        1112        1212        1299        1408         1457
+ranges = [[100,115],[115,126],[205,230],[300,320],[605,625],[730,750],[853,870],[910,930],[1020,1035],[1100,1118],[1700,1730],[1925,1950],[2145,2170],[2370,2425],[2480,2520],[2680,2720],[2740,2790],[3000,3030],[3200,3255],[3460,3540],[3590,3665]]
 energies= unp.uarray([0,0,0,121.7817,244.6974,295.9387,344.2785,367.7891,411.1165,443.9606,678.623,778.9045,867.380,964.057,1005.27,1085.837,1112.076,1212.948,1299.142,1408.013,1457.643],[0,0,0,0.0003,0.0008,0.0017,0.0012,0.0020,0.0012,0.0016,0.005,0.0024,0.003,0.005,0.05,0.010,0.003,0.011,0.008,0.003,0.011])
 wahrscheinlichkeiten = unp.uarray([0,0,0,107.3,28.39,1.656,100.0,3.232,8.413,10.63,1.777,48.62,15.90,54.57,2.48,38.04,51.40,5.320,6.14,78.48,1.869],[0,0,0,0.4,0.10,0.015,0.6,0.015,0.026,0.03,0.012,0.22,0.09,0.13,0.04,0.10,0.23,0.021,0.03,0.13,0.014])
-wahrscheinlichkeiten*= unp.uarray(0.2659,0.0013)
+wahrscheinlichkeiten*= unp.uarray(0.2659,0.0013)/100
 EU152 = np.genfromtxt('scripts/EU152',unpack=True)
 print('EU152')
 Plot(EU152,[[1,8192],[1,2000],[2000,4000],[1800,4000],[4000,8192]],'EU152')
@@ -148,19 +148,98 @@ EU152Params=gausFitMitPlot(EU152,ranges,'EU152')
 print(EU152Params)
 pos=[]
 posStd=[]
+sigma=[]
+sigmaStd=[]
+a=[]
+aStd=[]
 for params in EU152Params:
     pos.append(unp.nominal_values(params[3]))
     posStd.append(unp.std_devs(params[3]))
+    sigma.append(unp.nominal_values(params[2]))
+    sigmaStd.append(unp.std_devs(params[2]))
+    a.append(unp.nominal_values(params[0]))
+    aStd.append(unp.std_devs(params[0]))
+
 pos=np.array(pos)
 posStd=np.array(posStd)
+posU=unp.uarray(pos,posStd)
+sigma=np.array(sigma)
+sigmaStd=np.array(sigmaStd)
+sigmaU=unp.uarray(sigma,sigmaStd)
+a=np.array(a)
+aStd=np.array(aStd)
+aU=unp.uarray(a,aStd)
+
+x=np.linspace(1,8192,1000)
+params, error, sigmay = linregress(pos[wahrscheinlichkeiten!=0], unp.nominal_values(energies[wahrscheinlichkeiten!=0]))
+umrechnungsParams=unp.uarray(params,error)
+print(umrechnungsParams)
 plt.cla()
 plt.clf()
-plt.plot(pos, unp.nominal_values(energies), 'gx', label='Werte')  
+plt.plot(pos[wahrscheinlichkeiten!=0], unp.nominal_values(energies[wahrscheinlichkeiten!=0]), 'gx', label='Werte') 
+plt.plot(x, Line(x, *params), 'b-', label='Fit') 
 #plt.xlabel(r'$v$')
 #plt.ylabel(r'$\Delta f / \si{\hertz}$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/EnergieKali.pdf') 
+
+
+#def polynom3(x,a,b,c,d,f,g):
+#    return g*x**5+f*x**4+d*x**3+a*x**2+b*x+c
+
+#def polynom3(x,a,b,c,d):
+#    return d*x**3+a*x**2+b*x+c
+
+#def polynom3(x,a,b,c):
+#    return a*x**2+b*x+c
+
+def polynom3(x,a,b,c,d,f):
+    return f*x**4+d*x**3+a*x**2+b*x+c
+
+def expC(x,a,b):
+    return a*np.exp(-(x/1000-b))
+
+a=7.31 #cm
+r=2.25 #cm
+print('omega/4pi')
+omegaDurch4PI = (1-a/np.sqrt(a**2+r**2))/2
+print(omegaDurch4PI)
+AnzahlAnTagen=unp.uarray(6462,1) #days
+HalbwertsZeit=unp.uarray(4943,5) #days
+AktivitätEu=unp.uarray(4130,60) #bq
+AktivitätEu=1/2**(AnzahlAnTagen/HalbwertsZeit) * AktivitätEu
+print(AktivitätEu)
+
+print(np.sum(EU152)/(AktivitätEu*omegaDurch4PI*4740))
+x=np.linspace(0,3700,1000)
+inhalt=aU*(np.sqrt(2*np.pi)*sigmaU)
+xA=posU[wahrscheinlichkeiten!=0]
+yA=inhalt[wahrscheinlichkeiten!=0]/(AktivitätEu*omegaDurch4PI*4740*wahrscheinlichkeiten[wahrscheinlichkeiten!=0])
+xA=Line(xA[1:],*unp.nominal_values(umrechnungsParams))
+yA=yA[1:]
+params, covar = curve_fit(polynom3, unp.nominal_values(xA), unp.nominal_values(yA),sigma=unp.std_devs(yA))
+paramsEQU=unp.uarray(params, np.sqrt(np.diag(covar)))
+print(paramsEQU)
+plt.cla()
+plt.clf()
+plt.plot(x, polynom3(x, *params), 'r-', label='Fit') 
+plt.errorbar(unp.nominal_values(xA), unp.nominal_values(yA), yerr=unp.std_devs(yA), xerr=unp.std_devs(xA), label='Werte',fmt='x', capthick=0.5, linewidth='0.5',ecolor='b',capsize=1,markersize=1.5) 
+plt.xlim(150,1500)
+plt.ylim(0,0.3)
+plt.xlabel(r'$E_\gamma$/\si{\kilo\electronvolt}')
+plt.ylabel(r'$Q$')
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/Q.pdf') 
+
+######################################################
+
+
+
+
+
+
 
 
 Cs137 = np.genfromtxt('scripts/Cs137',unpack=True)
@@ -169,3 +248,14 @@ print('Cs137')
 print(gausFitMitPlot(Cs137,ranges,'Cs137'))
 
 #0.2659 13 
+
+########################################################Ba
+ranges = [[1,8192]]
+D = np.genfromtxt('scripts/D',unpack=True)
+print(gausFitMitPlot(D,ranges,'D'))
+
+
+########################################################?c060?
+ranges = [[1,8192]]
+D = np.genfromtxt('scripts/unbekannt',unpack=True)
+print(gausFitMitPlot(D,ranges,'unbekannt'))
