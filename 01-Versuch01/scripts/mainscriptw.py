bfc41d5ae05f08640d5842634382afa2d1054443
Lukas Rolf <lukas.rolf@online.de>
2018-07-08 17:56:33 +0200

    ok korrektur weiter geführt

--- 01-Versuch01/scripts/mainscriptw.py
+++ 01-Versuch01/scripts/mainscriptw.py
@@ -10,6 +10,7 @@ from scipy import stats
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import uncertainties.unumpy as unp
+import uncertainties
 from uncertainties import ufloat
 import scipy.constants as const
 from errorfunkt2tex import error_to_tex
@@ -45,8 +46,8 @@ peakPosstd=np.array(peakPosstd)
 peakPos=unp.uarray(peakPosnom,peakPosstd)  
 time=np.linspace(0.9,0.9+len(peakPosnom)-1,len(peakPosnom)) #in microsek
 makeNewTable([convert(peakPos,unpFormat,[r'','1.2f',True]),time],r'\multicolumn{1}{c}{Kanal} & {T/\si{\micro\second}}','tab1', [r'S', r'S'])
-params, error, sigmay = linregress(peakPosnom,time)
-fitparamsKal=unp.uarray(params,error)
+params, covar = curve_fit(line,peakPosnom,time)
+fitparamsKal=uncertainties.correlated_values(params, covar)
 EineMicroSekInChan = 1/fitparamsKal[0]
 print('Parameter Kalibrierung:')
 print(fitparamsKal)
@@ -102,9 +103,9 @@ params3, covar3 = curve_fit(expC, line(Channel[9:-37], *params), CountsZusammen[
 params4, covar4 = params3, covar3
 for i in range(1,1000):
     params4, covar4 = curve_fit(expC, line(Channel[9:-37], *params), CountsZusammen[9:-37],sigma=1/np.abs(CountsZusammen[9:-37]-expC(line(Channel[9:-37], *params),*params4)),maxfev=100000)
-fitparamsLebens1=unp.uarray(params2,np.sqrt(np.diag(covar2)))
-fitparamsLebens2=unp.uarray(params3,np.sqrt(np.diag(covar3)))
-fitparamsLebens3=unp.uarray(params4,np.sqrt(np.diag(covar4)))
+fitparamsLebens1=uncertainties.correlated_values(params2, covar2)
+fitparamsLebens2=uncertainties.correlated_values(params3, covar3)
+fitparamsLebens3=uncertainties.correlated_values(params4, covar4)
 print('Parameter Lebensdauer:')
 print(fitparamsLebens1)
 print('Lebensdauer in microsekunden:', 1/fitparamsLebens1[0])
@@ -124,8 +125,8 @@ plt.clf()
 plt.errorbar(line(Channel[9:-37], *params), CountsZusammen[9:-37],yerr=np.sqrt(CountsZusammen[9:-37]), label='In den Fit einbezogene Messwerte',fmt='x', capthick=0.5, linewidth='0.5',ecolor='b',capsize=1,markersize=1.5)
 plt.errorbar(line(Channel[1:9], *params), CountsZusammen[1:9],yerr=np.sqrt(CountsZusammen[1:9]), label='Messwerte mit Rauschen',fmt='x', capthick=0.5, linewidth='0.5',ecolor='g',color='darkgreen',capsize=1,markersize=1.5)
 plt.plot(line(x, *params), expC(line(x, *params),*params2), 'r-', label='Fit ungewichtet',linewidth='0.5')
-plt.plot(line(x, *params), expC(line(x, *params),*params3), 'k-', label='Fit gewichtet',linewidth='0.5')
-plt.plot(line(x, *params), expC(line(x, *params),*params4), 'y-', label='Fit iterativ gewichtet',linewidth='0.5')
+#plt.plot(line(x, *params), expC(line(x, *params),*params3), 'k-', label='Fit gewichtet',linewidth='0.5')
+#plt.plot(line(x, *params), expC(line(x, *params),*params4), 'y-', label='Fit iterativ gewichtet',linewidth='0.5')
 plt.xlim(0, line(x[-1], *params))
 plt.yscale('log')
 plt.ylabel(r'$N$')
@@ -138,16 +139,18 @@ def constante(x,c):
 	return x*0+c
 
 koinz=np.genfromtxt('scripts/koinz',unpack=True)
-makeNewTable(convert([*koinz],floatFormat,[r'','3.0f',False]),r'{$\varDelta t/\si{\nano\second}$} & {$N$}','tab2', [r'S[table-format=3.0]', r'S[table-format=3.0]'])
+t=10
+koinz[1]/=t
+makeNewTable(convert([koinz[0]],floatFormat,[r'','2.0f',False])+convert([koinz[1]],floatFormat,[r'','2.1f',False]),r'{$\varDelta t/\si{\nano\second}$} & {$A/\si[per-mode=reciprocal]{\per\second}$}','tab2', [r'S[table-format=2.0]', r'S[table-format=2.1]'])
 koinz1=[koinz[0][3:9],koinz[1][3:9]]
-koinz2=[koinz[0][11:17],koinz[1][11:17]]
+koinz2=[koinz[0][10:18],koinz[1][10:18]]
 koinz3=[koinz[0][19:24],koinz[1][19:24]]
-params1, error1, sigmay1 = linregress(*koinz1)
+params1, covar1 = curve_fit(line,*koinz1)
 params2, error2 = avg_and_sem(koinz2[1])
-params3, error3, sigmay3 = linregress(*koinz3)
-params1U=unp.uarray(params1,error1)
+params3, covar3 = curve_fit(line,*koinz3)
+params1U=uncertainties.correlated_values(params1, covar1)
 params2U=unp.uarray(params2,error2)
-params3U=unp.uarray(params3,error3)
+params3U=uncertainties.correlated_values(params3, covar3)
 print('Params1:',params1U)
 print('Params2:',params2U)
 print('Params3:',params3U)
@@ -155,7 +158,7 @@ x1=(params2U/2-params1U[1])/params1U[0]
 x2=(params2U/2-params3U[1])/params3U[0]
 print('x1:',x1)
 print('x2:',x2)
-print('deltatk:',x2-x1-40)
+print('deltatk:',40-(x2-x1))
 x=np.linspace(-30,30,1000)
 plt.cla()
 plt.clf()
@@ -168,9 +171,9 @@ mm6,=plt.plot(x,constante(x,params2), 'b-', linewidth='0.5')
 #plt.plot(x,constante(x,params2/2), 'k-', linewidth='0.5')
 mm7,=plt.plot(x,line(x,*params3), 'g-', label='Fit',linewidth='0.5')
 plt.xlim(-30, 30)
-plt.ylim(0, 300)
+plt.ylim(0, 30)
 #plt.yscale('log')
-plt.ylabel(r'$N$')
+plt.ylabel(r'$A/\si{\per\second}$')
 plt.xlabel(r'$\varDelta t/\si{\nano\second}$')
 plt.legend([(mm1, mm2, mm3, mm4), mm5, mm6, mm7], ['Wertepaare','Fit der linken Flanke','Fit des Plateaus','Fit der rechten Flanke'],handler_map={tuple: HandlerTuple(ndivide=None)},loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
